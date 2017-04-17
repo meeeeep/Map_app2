08b7a6350aaea60e02b4f37ce18f93b2e85ad9c2
Stephanie Cray <stephanie.cray@yahoo.com>
2017-04-17 12:43:25 -0400

    adding in addresslisting

---
+++ components/addresslist.js
@@ -0,0 +1,24 @@
+mport React, {Component} from 'react';
+import ReactNative from 'react-native';
+const styles = require('../styles.js')
+const { ScrollView, TouchableHighlight, Text } = ReactNative;
+
+var ListItem = React.createClass({
+	render: function(){
+	 return (
+      <TouchableHighlight onPress={this.props.onPress}>
+        <ScrollView style={styles.li}>
+          <Text style={styles.liText}>
+            {this.props.item.address}
+
+            </Text>
+        </ScrollView>
+      </TouchableHighlight>
+          
+     );
+
+	}
+})
+
+
+module.exports = ListItem;
